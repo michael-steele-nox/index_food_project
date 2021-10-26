38210e6134cb8861ee59c33d5af1150136a92d9f
michael-steele-nox <michael.steele.nox@gmail.com>
2021-10-26 15:43:58 +0300

    Added new blocks
    Added new blocks

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
-var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 10,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-      });
-var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 10,
-        thumbs: {
-          swiper: swiper,
-        },
-      });
-const menuBtn = document.querySelector(".menu-button");
-const menu = document.querySelector(".navbar-panel");
-const menuCloseBtn = document.querySelector(".close-menu");
-menuBtn.addEventListener("click", () => {
-	menu.classList.toggle("is-open");
-});
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
 
-menuCloseBtn.addEventListener("click", () => {
-	menu.classList.toggle("is-open");
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+  
 });
